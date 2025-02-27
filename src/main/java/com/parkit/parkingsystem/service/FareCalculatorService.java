831498f7140cfbff93c7d0e8ada800a1894413f3
Nikkune <dev.2tvhg@passmail.net>
2025-02-27 13:33:04 +0100

    Implement thirty minutes free

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,9 +5,9 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+    public void calculateFare(Ticket ticket) {
+        if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+            throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
         }
 
         long inHour = ticket.getInTime().getTime();
@@ -16,16 +16,23 @@ public class FareCalculatorService {
         long durationInMillis = outHour - inHour;
         float duration = (float) durationInMillis / 1000 / 60 / 60;
 
-        switch (ticket.getParkingSpot().getParkingType()){
+        switch (ticket.getParkingSpot().getParkingType()) {
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                if (duration > 0.5)
+                    ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                else
+                    ticket.setPrice(0);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                if (duration > 0.5)
+                    ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                else
+                    ticket.setPrice(0);
                 break;
             }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
+            default:
+                throw new IllegalArgumentException("Unkown Parking Type");
         }
     }
 }
