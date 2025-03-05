4bea852977f03315168d104e1b4a31c5d31de11f
Nikkune <dev.2tvhg@passmail.net>
2025-03-05 15:59:37 +0100

    Add ParkingService Test

--- src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -57,6 +57,37 @@ public class ParkingServiceTest {
     }
 
     @Test
+    public void processIncomingVehicleTestRegularUser() throws Exception {
+        // Arrange
+        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+        when(ticketDAO.getNbTicket(anyString())).thenReturn(2);
+        when(parkingSpotDAO.getNextAvailableSlot(any(ParkingType.class))).thenReturn(1);
+        when(inputReaderUtil.readSelection()).thenReturn(1);
+
+        // Act
+        parkingService.processIncomingVehicle();
+
+        // Assert
+        verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
+        verify(ticketDAO, Mockito.times(1)).saveTicket(any(Ticket.class));
+        verify(ticketDAO, Mockito.times(1)).getNbTicket(anyString());
+    }
+
+    @Test
+    public void processIncomingVehicleTestUnableProcess() throws Exception {
+        // Arrange
+        when(parkingSpotDAO.getNextAvailableSlot(any(ParkingType.class))).thenReturn(1);
+        when(inputReaderUtil.readSelection()).thenReturn(1);
+        doThrow(Exception.class).when(inputReaderUtil).readVehicleRegistrationNumber();
+
+        // Act
+        parkingService.processIncomingVehicle();
+
+        // Assert
+        verify(parkingSpotDAO, never()).updateParking(any(ParkingSpot.class));
+    }
+
+    @Test
     public void processExitingVehicleTest() throws Exception {
         // Arrange
         when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
@@ -75,6 +106,29 @@ public class ParkingServiceTest {
         // Assert
         verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
         verify(ticketDAO, Mockito.times(1)).updateTicket(any(Ticket.class));
+    }
+
+    @Test
+    public void processExitingVehicleTestRegularUser() throws Exception {
+        // Arrange
+        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+        when(ticketDAO.getNbTicket(anyString())).thenReturn(2);
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+        Ticket ticket = new Ticket();
+        ticket.setInTime(new Date(System.currentTimeMillis() - (60 * 60 * 1000)));
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setVehicleRegNumber("ABCDEF");
+        when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
+        when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
+        when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
+
+        // Act
+        parkingService.processExitingVehicle();
+
+        // Assert
+        verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
+        verify(ticketDAO, Mockito.times(1)).updateTicket(any(Ticket.class));
+        verify(ticketDAO, Mockito.times(1)).getNbTicket(anyString());
     }
 
     @Test
